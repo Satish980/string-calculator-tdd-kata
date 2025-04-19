737df3993b57f85b8293c4bd82153a221cc4dc2d
satish980 <isatishkumar4008@gmail.com>
2025-04-19 15:01:02 +0530

    test cased added for step 3

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,11 +1,15 @@
 const assert = require('assert');
 const { add } = require('../src/calculator');
 
-// Empty string should return 0
+// STEP 1: Empty string should return 0
 assert.strictEqual(add(""), 0);
 
-// Single number returns that number
+// STEP 2: Single number returns that number
 assert.strictEqual(add("1"), 1);
+
+// STEP 3: Two numbers comma separated
+assert.strictEqual(add("1,2"), 3);
+
 
 console.log("All tests passed!");
 
