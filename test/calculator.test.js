95888826fad1cb058d6ff10d667091c8ebd63849
satish980 <isatishkumar4008@gmail.com>
2025-04-19 14:58:51 +0530

    Test: Single number returns that number - step 2

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,7 +1,11 @@
 const assert = require('assert');
 const { add } = require('../src/calculator');
 
+// Empty string should return 0
 assert.strictEqual(add(""), 0);
+
+// Single number returns that number
+assert.strictEqual(add("1"), 1);
 
 console.log("All tests passed!");
 
