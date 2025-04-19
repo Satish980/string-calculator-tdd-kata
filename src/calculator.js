72cb0dadd2b80e062e41fd74f881c8358e0a7806
satish980 <isatishkumar4008@gmail.com>
2025-04-19 15:25:15 +0530

    feat: support custom delimiters of any length

--- src/calculator.js
+++ src/calculator.js
@@ -4,7 +4,17 @@ function add(numbers) {
   let delimiter = /,|\n/;
   if (numbers.startsWith("//")) {
     const delimiterLineEnd = numbers.indexOf("\n");
-    delimiter = new RegExp(numbers.slice(2, delimiterLineEnd));
+    const delimiterSection = numbers.slice(2, delimiterLineEnd);
+
+    if (delimiterSection.startsWith("[")) {
+      const matches = [...delimiterSection.matchAll(/\[([^\]]+)\]/g)];
+      const delimiters = matches.map(m => m[1].replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'));
+      delimiter = new RegExp(delimiters.join("|"));
+    }
+    else {
+      delimiter = new RegExp(numbers.slice(2, delimiterLineEnd));
+    }
+
     numbers = numbers.slice(delimiterLineEnd + 1);
   }
 
