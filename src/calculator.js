0d2b506c9640ce6bd8ee394b0382ff1d90248be5
satish980 <isatishkumar4008@gmail.com>
2025-04-19 15:07:48 +0530

    feat: support custom single character delimiters

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,15 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  const parts = numbers.split(/,|\n/); // split on comma or newline
+
+  let delimiter = /,|\n/;
+  if (numbers.startsWith("//")) {
+    const delimiterLineEnd = numbers.indexOf("\n");
+    delimiter = new RegExp(numbers.slice(2, delimiterLineEnd));
+    numbers = numbers.slice(delimiterLineEnd + 1);
+  }
+
+  const parts = numbers.split(delimiter);
   return parts.reduce((sum, num) => sum + parseInt(num), 0);
 }
+
 module.exports = { add };
