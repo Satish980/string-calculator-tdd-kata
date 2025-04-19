282c90f9d4c928def501d4f3621399ef5904b09f
satish980 <isatishkumar4008@gmail.com>
2025-04-19 15:03:19 +0530

    test: newlines as delimiters test case added

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -13,5 +13,8 @@ assert.strictEqual(add("1,2"), 3);
 // STEP 4: Any number of comma-separated values
 assert.strictEqual(add("1,2,3,4"), 10);
 
+// STEP 5: Handle newlines as delimiters
+assert.strictEqual(add("1\n2,3"), 6);
+
 console.log("All tests passed!");
 
